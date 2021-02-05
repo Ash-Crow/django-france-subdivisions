b02dc37c2cb487ce3fc7cd595bf92d7ca39b0f74
Ash_Crow <sylvain@ashtree.eu>
2021-02-05 16:29:08 +0100

    [API] Changed API engine from Django REST Framework to Django Ninja

--- francesubdivisions/urls.py
+++ francesubdivisions/urls.py
@@ -1,49 +1,11 @@
+from ninja import NinjaAPI
 from django.urls import path
-from rest_framework.urlpatterns import format_suffix_patterns
-from francesubdivisions import views
+from francesubdivisions.api import router as fs_router
+
+api = NinjaAPI()
+
+api.add_router("/", fs_router)
 
 urlpatterns = [
-    path("all/<str:query>", views.SearchAll, name="search-all"),
-    path("regions/", views.RegionList.as_view(), name="region-list"),
-    path("regions/<int:pk>/", views.RegionDetail.as_view(), name="region-detail"),
-    path(
-        "regions/siren/<int:siren>/",
-        views.RegionDetailSiren.as_view(),
-        name="region-detail-siren",
-    ),
-    path("departements/", views.DepartementList.as_view(), name="departement-list"),
-    path(
-        "departements/<int:pk>/",
-        views.DepartementDetail.as_view(),
-        name="departement-detail",
-    ),
-    path(
-        "departements/siren/<int:siren>/",
-        views.DepartementDetailSiren.as_view(),
-        name="departement-detail-siren",
-    ),
-    path("epci/", views.EpciList.as_view(), name="epci-list"),
-    path("epci/<int:pk>/", views.EpciDetail.as_view(), name="epci-detail"),
-    path(
-        "epci/siren/<int:siren>/",
-        views.EpciDetailSiren.as_view(),
-        name="epci-detail-siren",
-    ),
-    path("communes/", views.CommuneList.as_view(), name="commune-list"),
-    path("communes/<int:pk>/", views.CommuneDetail.as_view(), name="commune-detail"),
-    path(
-        "communes/siren/<int:siren>/",
-        views.CommuneDetailSiren.as_view(),
-        name="commune-detail-siren",
-    ),
-    path(
-        "communes/insee/<int:insee>/",
-        views.CommuneDetailInsee.as_view(),
-        name="commune-detail-insee",
-    ),
-    path("datayear/", views.DataYearList.as_view(), name="datayear-list"),
-    path("datayear/<int:pk>/", views.DataYearDetail.as_view(), name="datayear-detail"),
-    path("", views.api_root),
+    path("api/", api.urls),
 ]
-
-urlpatterns = format_suffix_patterns(urlpatterns)
