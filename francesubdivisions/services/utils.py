2dbc49a2abfc2a3cfd27018e6a76e88cc27407d8
Ash_Crow <sylvain@ashtree.eu>
2021-07-13 17:04:38 +0200

    [utils] Reorganized functions for ease of testing

--- francesubdivisions/services/utils.py
+++ francesubdivisions/services/utils.py
@@ -1,30 +1,44 @@
 import csv
+from typing import Callable
 import requests
 from zipfile import ZipFile
 from io import BytesIO, TextIOWrapper
 
 
-def parse_csv_from_distant_zip(zip_url, csv_name, column_names, typecheck=False):
-    print(f"🗜️   Parsing archive {zip_url}")
-    zip_name = requests.get(zip_url).content
-    with ZipFile(BytesIO(zip_name)) as zip_file:
+def parse_csv_from_distant_zip(
+    zip_url: str,
+    get_zip_method: Callable[[str], ZipFile],
+    csv_name: str,
+    column_names: dict,
+    typecheck: dict = False,
+) -> list:
+    with get_zip_method(zip_url) as zip_file:
         with zip_file.open(csv_name) as csv_file:
-            reader = csv.DictReader(TextIOWrapper(csv_file, encoding="utf-8-sig"))
-            entries = []
-            if typecheck:
-                tc_col = typecheck["column"]
-                tc_val = typecheck["value"]
+            stream = TextIOWrapper(csv_file, encoding="utf-8-sig")
+            return parse_csv_from_stream(stream, column_names, typecheck)
 
-            for row in reader:
-                if typecheck:
-                    if row[tc_col] != tc_val:
-                        continue
-                entry = {}
-                # Parse all the columns
-                for key, val in column_names.items():
-                    entry[key] = row[val]
-                entries.append(entry)
-            return entries
+
+def parse_csv_from_stream(
+    stream,
+    column_names: dict,
+    typecheck: dict = False,
+):
+    reader = csv.DictReader(stream)
+    entries = []
+    if typecheck:
+        tc_col = typecheck["column"]
+        tc_val = typecheck["value"]
+
+    for row in reader:
+        if typecheck:
+            if row[tc_col] != tc_val:
+                continue
+        entry = {}
+        # Parse all the columns
+        for key, val in column_names.items():
+            entry[key] = row[val]
+        entries.append(entry)
+    return entries
 
 
 def add_sirens_and_categories(input_file, model_name, year_entry):
@@ -46,3 +60,14 @@ def add_sirens_and_categories(input_file, model_name, year_entry):
                     collectivity_entry.save()
                 except:
                     print(f"{model_name} {insee} not found")
+
+
+def file_exists_at_url(url: str) -> bool:
+    r = requests.head(url)
+    return r.status_code == requests.codes.ok
+
+
+def get_zip_from_url(zip_url: str) -> ZipFile:
+    zip_name = requests.get(zip_url).content
+    zip_file = ZipFile(BytesIO(zip_name))
+    return zip_file
